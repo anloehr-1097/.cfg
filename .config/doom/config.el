8088502326104d5bd3700988baae58d1cb009624
aloehr-1097 <Andy@amux.fritz.box>
2023-09-02 14:30:17 +0200

    Theme

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -34,8 +34,9 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-acario-dark)
-
+;;(setq doom-theme 'doom-acario-dark)
+(setq doom-theme 'gruber-darker)
+(set-background-color "black")
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type nil)
