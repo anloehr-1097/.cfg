274ecd397f4d03e407a69a0d05c3424880292dcc
aloehr-1097 <Andy@amux.fritz.box>
2023-09-28 13:28:18 +0200

    config gnu emacs, doom emacs on the side

---
+++ .config/doom.bu/custom_bu.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("7feeed063855b06836e0262f77f5c6d3f415159a98a9676d549bfeb6c49637c4" "e9d47d6d41e42a8313c81995a60b2af6588e9f01a1cf19ca42669a7ffd5c2fde" "5586a5db9dadef93b6b6e72720205a4fa92fd60e4ccfd3a5fa389782eab2371b" "1aa4243143f6c9f2a51ff173221f4fd23a1719f4194df6cef8878e75d349613d" "6945dadc749ac5cbd47012cad836f92aea9ebec9f504d32fe89a956260773ca4" "ba4ab079778624e2eadbdc5d9345e6ada531dc3febeb24d257e6d31d5ed02577" "2dd4951e967990396142ec54d376cced3f135810b2b69920e77103e0bcedfba9" default))
+ '(package-selected-packages
+   '(jupyter csv-mode csv hemisu-theme cyberpunk-theme gruber-darker-theme counsel-projectile flycheck-clang-analyzer vertico ivy-avy lsp-ivy counsel ivy-rich auto-virtualenv lsp-pyright flycheck-mypy emacsql-sqlite org-noter exwm @ dap-mode flycheck))
+ '(pdf-tools-handle-upgrades t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
