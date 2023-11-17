d7524328789f22cabfd1d191464cc8a26d2d8f95
aloehr-1097 <andreas.loehr97@gmail.com>
2023-11-17 14:21:58 +0100

    nvim config

--- .config/nvim/lua/andy/plugins/harpoon.lua
+++ .config/nvim/lua/andy/plugins/harpoon.lua
@@ -1,24 +1,17 @@
 return {
-  "ThePrimeagen/harpoon",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-  config = function()
-    -- set keymaps
-    local keymap = vim.keymap -- for conciseness
+	"ThePrimeagen/harpoon",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	config = function()
+		-- set keymaps
+		local keymap = vim.keymap -- for conciseness
+		local mark = require("harpoon.mark")
+		local ui = require("harpoon.ui")
 
-    keymap.set(
-      "n",
-      "<leader>hm",
-      "<cmd>lua require('harpoon.mark').add_file()<cr>",
-      { desc = "Mark file with harpoon" }
-    )
-    keymap.set("n", "<leader>hn", "<cmd>lua require('harpoon.ui').nav_next()<cr>", { desc = "Go to next harpoon mark" })
-    keymap.set(
-      "n",
-      "<leader>hp",
-      "<cmd>lua require('harpoon.ui').nav_prev()<cr>",
-      { desc = "Go to previous harpoon mark" }
-    )
-  end,
+		keymap.set("n", "<leader>hm", mark.add_file, { desc = "Mark file with harpoon" })
+		keymap.set("n", "<leader>hn", ui.nav_next, { desc = "Go to next harpoon mark" })
+		keymap.set("n", "<leader>hp", ui.nav_prev, { desc = "Go to previous harpoon mark" })
+		keymap.set("n", "<C-e>", ui.toggle_quick_menu, { desc = "Open harpoon menu." })
+	end,
 }
