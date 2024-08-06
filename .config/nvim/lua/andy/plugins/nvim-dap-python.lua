6c078f1a9a1b6aba20f9fc77949e5c3b40705d10
aloehr-1097 <andreas.loehr97@gmail.com>
2024-08-05 23:29:34 -0400

    nvim dap python config

--- .config/nvim/lua/andy/plugins/nvim-dap-python.lua
+++ .config/nvim/lua/andy/plugins/nvim-dap-python.lua
@@ -7,8 +7,42 @@ return {
 			"rcarriga/nvim-dap-ui",
 		},
 		config = function(_, opts)
-			local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
-			require("dap-python").setup(path)
+
+            local venv_path = os.getenv('VIRTUAL_ENV')
+            local venv_py
+            local dap_config
+            if venv_path then
+                venv_py = venv_path .. '/bin/python'
+            else
+                venv_py = '/usr/bin/python3'
+            end
+
+            local cwd = vim.fn.getcwd()
+            local dap_config_file = cwd .. '/.nvim/dap.lua'
+            if vim.fn.filereadable(dap_config_file) == 1 then
+                dap_config = dofile(dap_config_file)
+            end
+
+            --local dap_python = require('nvim-dap-python')
+            --dap_python.setup(venv_py)
+
+            require('dap-python').setup(venv_py)
+            table.insert(require('dap').configurations.python, {
+                type = 'python',
+                request = 'launch',
+                name = "Launch " .. (dap_config.module or "Module");
+                module = dap_config.module or function()
+                    return vim.fn.input('Module name to debug: ')
+                end;
+                args = dap_config.args or function()
+                    return vim.fn.input('Arguments: ', ''):split(" ")
+                end;
+                -- ... more options, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings
+            })
+
+
+			-- local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
+			-- require("dap-python").setup(path)
 			local keymap = vim.keymap
 			local dap_py = require("dap-python")
 
@@ -23,5 +57,6 @@ return {
 				end, opts_map)
 			end
 		end,
-	},
+    }
+
 }
