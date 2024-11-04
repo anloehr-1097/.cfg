ca5fb678d44c8b37469b62305f976591510e9c9b
aloehr-1097 <andreas.loehr97@gmail.com>
2024-11-04 21:52:16 +0100

    sync

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -526,7 +526,7 @@
 (use-package org-bullets
   :hook (org-mode . org-bullets-mode)
   :custom
-  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+  (org-bullets-bullet-list '("♔" "♕" "♜" "♝" "♞" "♟" "♻")))
 
 ;;(use-package org-superstar
     ;;:ensure t
@@ -540,10 +540,10 @@
 (use-package visual-fill-column
   :hook (org-mode . efs/org-mode-visual-fill))
 
-(use-package org-modern
-  :after org
-  :ensure t
-  :hook (org-mode . org-modern-mode))
+;(use-package org-modern
+  ;:after org
+  ;:ensure t
+  ;:hook (org-mode . org-modern-mode))
 
 (with-eval-after-load 'org
   (org-babel-do-load-languages
@@ -684,8 +684,14 @@
 (use-package conda
   :ensure t)
 (custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(conda-anaconda-home "/usr/local/Caskroom/miniconda/base/")
- )
+ '(custom-safe-themes
+   '("e27c9668d7eddf75373fa6b07475ae2d6892185f07ebed037eedf783318761d7" default))
+ '(pdf-tools-handle-upgrades t))
 
 (use-package cuda-mode
   :ensure t)
@@ -992,3 +998,9 @@
 
 (evil-define-key 'normal 'global (kbd "<leader>cl")
   'load-init-file)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
