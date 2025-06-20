aa1c542795d99431182f3677de1df4d27056eea6
Andreas Loehr <andreas.loehr97@gmail.com>
2025-06-20 09:43:36 +0200

    nvim surroud, nvim-dap venv, flash config

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -817,8 +817,10 @@
    '("4594d6b9753691142f02e67b8eb0fda7d12f6cc9f1299a49b819312d6addad1d"
      default))
  '(org-agenda-files
-   '("~/research/reading_list.org" "/Users/anlhr/org/Tasks.org"
-     "/Users/anlhr/org/Habits.org" "/Users/anlhr/org/Birthdays.org"
+   '("~/org-roam/li24_top_erl.org"
+     "/Users/anlhr/research/reading_list.org"
+     "/Users/anlhr/org/Tasks.org" "/Users/anlhr/org/Habits.org"
+     "/Users/anlhr/org/Birthdays.org"
      "/Users/anlhr/KeepInSync/Life.org"
      "/Users/anlhr/research/planning.org"))
  '(pdf-tools-handle-upgrades t))
--- .config/nvim/lua/andy/plugins/nvim-dap-python.lua
+++ .config/nvim/lua/andy/plugins/nvim-dap-python.lua
@@ -1,16 +1,16 @@
 local function load_dap_configs()
-    local cwd = vim.fn.getcwd()
-    local dap_config_file = cwd .. '/.nvim/dap.lua'
-    if vim.fn.filereadable(dap_config_file) == 1 then
-        local dap_config = dofile(dap_config_file)
+	local cwd = vim.fn.getcwd()
+	local dap_config_file = cwd .. "/.nvim/dap.lua"
+	if vim.fn.filereadable(dap_config_file) == 1 then
+		local dap_config = dofile(dap_config_file)
 
-        for _, config in ipairs(dap_config) do
-            table.insert(require('dap').configurations.python, config)
-        end
-    end
-end;
+		for _, config in ipairs(dap_config) do
+			table.insert(require("dap").configurations.python, config)
+		end
+	end
+end
 -- Create a command to call the function from command mode in nvim
-vim.api.nvim_create_user_command('LoadDapConfig', load_dap_configs , {});
+vim.api.nvim_create_user_command("LoadDapConfig", load_dap_configs, {})
 
 return {
 	{
@@ -21,24 +21,23 @@ return {
 			"rcarriga/nvim-dap-ui",
 		},
 		config = function(_, opts)
-            local venv_path = os.getenv('VIRTUAL_ENV')
-            local venv_py
-            local dap_config
-            if venv_path then
-                venv_py = venv_path .. '/bin/python'
-            else
-                venv_py = '/usr/bin/python3'
-            end
+			local venv_path = os.getenv("VIRTUAL_ENV")
+			local conda_prefix = os.getenv("CONDA_PREFIX")
 
-            -- local cwd = vim.fn.getcwd()
-            -- local dap_config_file = cwd .. '/.nvim/dap.lua'
-            -- if vim.fn.filereadable(dap_config_file) == 1 then
-            --     dap_config = dofile(dap_config_file)
-            --     Conf_name = dap_config.module
-            --     Module_name = dap_config.module
-            --     Args = dap_config.args
-            -- end
-            require('dap-python').setup(venv_py)
+			local venv_py
+			if venv_path then
+				venv_py = venv_path .. "/bin/python"
+			elseif conda_prefix then
+				venv_py = conda_prefix .. "/bin/python"
+			elseif os.getenv("PYTHONPATH") then
+				venv_py = "/usr/bin/python3"
+			else
+				venv_py = nil
+			end
+
+			if venv_py then
+				require("dap-python").setup(venv_py)
+			end
 
 			local keymap = vim.keymap
 			local dap_py = require("dap-python")
@@ -55,14 +54,12 @@ return {
 			end
 		end,
 
-         splitString = function(input)
-            local words = {}
-            for word in string.gmatch(input, "%S+") do
-                table.insert(words, word)
-            end
-            return words
-        end;
-
-    }
-
+		splitString = function(input)
+			local words = {}
+			for word in string.gmatch(input, "%S+") do
+				table.insert(words, word)
+			end
+			return words
+		end,
+	},
 }
