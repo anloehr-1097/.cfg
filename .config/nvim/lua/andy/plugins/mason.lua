c4ba1a3e5509b747cc67cd50474c885e0807e629
Andreas Loehr <andreas.loehr97@gmail.com>
2025-08-22 05:35:16 -0400

    sync with remote

---
+++ .config/nvim/lua/andy/plugins/mason.lua
@@ -0,0 +1,129 @@
+return {
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+        "hrsh7th/nvim-cmp",
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
+        "j-hui/fidget.nvim",
+	},
+	config = function()
+		-- import mason
+		local mason = require("mason")
+
+		-- import mason-lspconfig
+		local mason_lspconfig = require("mason-lspconfig")
+
+		local mason_tool_installer = require("mason-tool-installer")
+
+
+        local cmp = require('cmp')
+        local cmp_lsp = require("cmp_nvim_lsp")
+        local capabilities = vim.tbl_deep_extend(
+            "force",
+            {},
+            vim.lsp.protocol.make_client_capabilities(),
+            cmp_lsp.default_capabilities())
+
+		-- enable mason and configure icons
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
+
+		mason_lspconfig.setup({
+			-- list of servers for mason to install
+			ensure_installed = {
+				"lua_ls",
+				"pyright",
+				"clangd",
+			},
+            handlers = {
+                function(server_name) -- default handler (optional)
+
+                    if server_name == "clangd" then
+                        require("lspconfig")[server_name].setup {
+                            capabilities = capabilities,
+                            cmd = { "clangd", "--background-index", "--query-driver=/usr/bin/clang++" },
+                            filetypes = { "c", "cpp", "objc", "objcpp" },
+                            on_attach = function(client, bufnr)
+                                require("lsp_signature").on_attach()
+                            end
+                        }
+
+
+                    else
+
+                    require("lspconfig")[server_name].setup {
+                        capabilities = capabilities
+                    }
+                    end
+                end,
+
+                ["lua_ls"] = function()
+                    local lspconfig = require("lspconfig")
+                    lspconfig.lua_ls.setup {
+                        capabilities = capabilities,
+                        settings = {
+                            Lua = {
+                                runtime = { version = "Lua 5.1" },
+                                diagnostics = {
+                                    globals = { "vim", "it", "describe", "before_each", "after_each" },
+                                }
+                            }
+                        }
+                    }
+                end,
+
+                ["clangd"]  = function()
+                    require("lspconfig")["clangd"].setup {
+                        cmd = { "clangd", "--background-index", "--query-driver=/usr/bin/clang++" },
+                        filetypes = { "c", "cpp", "objc", "objcpp" },
+                    }
+                end,
+
+                ["pyright"] = function()
+                    require("lspconfig").pyright.setup {
+                        capabilities = capabilities,
+                        settings = {
+                            python = {
+                                analysis = {
+                                    autoSearchPaths = true,
+                                    useLibraryCodeForTypes = true,
+                                    diagnosticMode = "workspace",
+                                    typeCheckingMode = "basic",
+                                    autoImportCompletions = true,
+                                }
+                            }
+                        }
+                    }
+
+                end,
+            },
+
+			-- auto-install configured servers (with lspconfig)
+			automatic_installation = true, -- not the same as ensure_installed
+		})
+
+		mason_tool_installer.setup({
+			ensure_installed = {
+				--"prettier", -- prettier formatter
+				"stylua", -- lua formatter
+				"isort", -- python formatter
+				"black", -- python formatter
+				"pylint", -- python linter
+				"debugpy", -- python debugger
+			},
+		})
+	end,
+}
